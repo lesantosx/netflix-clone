4e3b60a956bc1a64095534a1fc35b7be2976dda1
lesantosx <leticia.santos1997@outlook.com>
2023-04-12 16:02:38 -0300

    Add icones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import './Header.css'
 
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import SearchIcon from '@mui/icons-material/Search'
+
 export default function Header({ black }) {
+
   return (
     <header className={black ? 'black' : ''}>
       <div className="header--logo">
         <a href="/">
           <img src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png" alt="" />
-        </a>        
+        </a> 
+        <div class="header--menu">
+          <a href="#home">Home</a>
+          <a href="#tvshows">TV Shows</a>
+          <a href="#movies">Movies</a>
+          <a href="#new&popular">New & Popular</a>
+          <a href="#mylist">My List</a>
+        </div>     
       </div>
+            
       <div className="header--user">
-          <a href="#">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
-          </a>
-        </div>
+        <SearchIcon sx={{ fontSize: 30 }}/>        
+        <NotificationsNoneIcon sx={{ fontSize: 30 }}/>          
+        <a href="#">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
+        </a>
+      </div>
     </header>
   )
 }
