59a0be4f86ec988b845b27068763aad2d2e482ee
lesantosx <leticia.santos1997@outlook.com>
2023-04-12 13:33:32 -0300

    Add new functions

--- src/App.js
+++ src/App.js
@@ -61,6 +61,11 @@ export default function App() {
 
       </section>
       <Footer />
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="loading"/>
+        </div>
+      }
     </div>
   );
 }
--- src/components/FeaturedMovie/index.jsx
+++ src/components/FeaturedMovie/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './FeaturedMovie.css'
-
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 
 export default function FeaturedMovie({ item }){
   let fistDate = new Date(item.first_air_date)
@@ -8,6 +9,12 @@ export default function FeaturedMovie({ item }){
 
   for(let i in item.genres){
     genres.push(item.genres[i].name)
+  }
+
+  let description = item.overview
+
+  if (description.length > 200){
+    description = description.substring(0, 280) + '...'
   }
 
   return (
@@ -25,10 +32,12 @@ export default function FeaturedMovie({ item }){
             <div className="featured--year">{fistDate.getFullYear()}</div>
             <div className="featured--seasons">{item.number_of_seasons} season{item.number_of_seasons !== 1 ? 's' : ''}</div>
           </div>
-          <div className="featured--description">{item.overview}</div>
+          <div className="featured--description">{description}</div>
           <div className="featured--buttons">
-            <a href={`/watch/${item.id}`} className="featured--playbtn">Play</a>
-            <a href={`/list/add/${item.id}`} className="featured--mylistbtn">More Info</a>
+            <a href={`/watch/${item.id}`} className="featured--playbtn">
+              <PlayArrowIcon /> Play
+              </a>
+            <a href={`/list/add/${item.id}`} className="featured--mylistbtn"><InfoOutlinedIcon /> More Info</a>
           </div>
           <div className="featured--genres">Genres: {genres.join(', ')}</div>
         </div>
