7324fe18fb0215aed329e02c8a23fd045931dc2f
Letícia Santos <leticia.santos1997@outlook.com>
2023-04-14 14:39:51 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,6 @@ export default function Footer(){
   return (
     <footer className="footer">
       Developed by <a href="https://github.com/lesantosx" target="_blank" rel="noreferrer">lesantosx</a>.<br />
-      is a simplified front end clone of Netflix
       This site is a simplified front end clone of Netflix platform, for study purposes.<br />
       Credits: Image rights for <a href="https://www.netflix.com/browse" target="_blank" rel="noreferrer">Netflix</a> and Data from <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">TheMoviedb.org</a> website.
     </footer>
